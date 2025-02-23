00616a610583fde513cd513bbac3790f6b9a25d8
achujoseph0709 <achujozefsl0709@gmail.com>
2025-02-23 20:28:01 +0530

    befor first deployment-added allowed host

--- getorgi/settings.py
+++ getorgi/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-9ha2zc$$+nju2!o6tvw!l9d%xm63solr!lnsetnitp_6ri^=y8
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*","88.222.241.142"]
 
 
 # Application definition
