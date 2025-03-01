426ba195fa7aa5aeec0a6acff2d80ef3f74007ff
achujoseph0709 <achujozefsl0709@gmail.com>
2025-03-02 01:50:08 +0530

    settings updated

--- getorgi/settings.py
+++ getorgi/settings.py
@@ -43,6 +43,8 @@ INSTALLED_APPS = [
     'mptt',
     'api',
     'web',
+    "user",
+
 ]
 
 MIDDLEWARE = [
@@ -89,7 +91,12 @@ DATABASES = {
         'PORT': '5432',
     }
 }
-
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
 REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': (
         'rest_framework_simplejwt.authentication.JWTAuthentication',
@@ -144,3 +151,5 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 AUTH_USER_MODEL = 'api.Customer'
+
+STATICFILES_DIRS = [BASE_DIR / "static"]
