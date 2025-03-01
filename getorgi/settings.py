9856560ae92aa8aa2c86dbeeeee52f1401f1755d
achujoseph0709 <achujozefsl0709@gmail.com>
2025-03-02 01:51:45 +0530

    static root set

--- getorgi/settings.py
+++ getorgi/settings.py
@@ -153,3 +153,5 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 AUTH_USER_MODEL = 'api.Customer'
 
 STATICFILES_DIRS = [BASE_DIR / "static"]
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
